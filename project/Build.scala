15d4ea90cb9fc302589db71b063f78834ed1f410
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2015-07-03 09:49:03 +0300

    Initial

---
+++ project/Build.scala
@@ -0,0 +1,22 @@
+import org.stormenroute.mecha._
+import sbt._
+import sbt.Keys._
+
+object DockerScalaBuild extends MechaRepoBuild {
+  lazy val dockerScalaSettings = Defaults.defaultSettings ++
+    MechaRepoPlugin.defaultSettings ++ Seq(
+    name := "docker-scala",
+    scalaVersion := "2.11.7",
+    version := "0.1",
+    organization := "com.cleawing",
+    libraryDependencies ++= superRepoDependencies("docker-scala")
+  )
+
+  def repoName = "docker-scala"
+
+  lazy val dockerScala: Project = Project(
+    "docker-scala",
+    file("."),
+    settings = dockerScalaSettings
+  ) dependsOnSuperRepo
+}
